818576f491cdc5bac9f7e27b52fabf5ebffd64ee
cwhitesmith <65391162+cwhitesmith@users.noreply.github.com>
2020-07-08 18:07:42 +0200

    Commit index.php

---
+++ LB3/web/index.php
@@ -0,0 +1,23 @@
+<html>
+    <head>
+        <title>Webserver</title>
+    </head>
+
+    <body>
+        <h1>Webserver</h1>
+        <ul>
+            <?php
+
+            $json = file_get_contents('http://content-service/');
+            $obj = json_decode($json);
+
+            $products = $obj->products;
+
+            foreach ($products as $product) {
+                echo "<li>$product</li>";
+            }
+
+            ?>
+        </ul>
+    </body>
+</html>
